e3c61dd29b401ed23b24d605349c5d52f10b84e0
osman ismail <osman.ism@live.nl>
2022-05-12 15:13:45 +0200

    update

--- weatherApp/components/SearchBar.js
+++ weatherApp/components/SearchBar.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import { View, TextInput, StyleSheet, Dimensions} from 'react-native'
+import { EvilIcons } from '@expo/vector-icons';
 
-export default function Weather() {
+
+export default function SearchBar({fetchWeatherData}) {
+
+  const [cityName, setCityName] = useState('');
+
   return (
-    <View>
+    <View style={styles.searchBar}>
+      <TextInput
+        style={{color: 'white', fontWeight: 'bold', width: Dimensions.get('screen').width -20 }}
+        placeholder='Enter City name'
+        value={cityName}
+        onChangeText={(text) => setCityName(text)
+       }
+      />
+
+      <EvilIcons name='search' size={28} color='black' onPress={() => fetchWeatherData(cityName)} />
 
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    searchBar: {
+      marginTop: 35,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      width: Dimensions.get('screen').width -20,
+      borderWidth: 1.5,
+      paddingVertical: 10,
+      paddingHorizontal: 10,
+      borderRadius: 25,
+      marginHorizontal: 10,
+      backgroundColor: 'rgba(0,0,0, 0.5)',
+      borderColor: 'rgba(0,0,0, 0.5)'
+
+      
+    },
+    textInput: {
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      width: Dimensions.get('screen').width -20,
+      
+      
+      
+      
+    }
+});
--- weatherApp/components/Weather.js
+++ weatherApp/components/Weather.js
@@ -4,13 +4,15 @@ import SearchBar from './SearchBar';
 import { haze, rainy, snow, sunny } from '../assets/background/index';
 
 
-export default function Weather({weatherData}) {
+export default function Weather({ weatherData, fetchWeatherData }) {
 
     const [backgroundImage, setBackgroundImage] = useState(null);
+    
     const { weather,
             name,
             main: { temp, humidity },
             wind: { speed }
+            
     } = weatherData;
     const [{ main }] = weather;
 
@@ -23,73 +25,172 @@ export default function Weather({weatherData}) {
         if(weather === 'Clear') return sunny
         if(weather === 'Rain') return rainy
         if(weather === 'Haze') return haze
-        return haze;
+        return haze;   
     }
 
-    var textColor = backgroundImage !== sunny ? 'white' : 'orange'; 
-    
-
-  return (
-    <View style={styles.container}>
-
-    <ImageBackground
-        source={backgroundImage}
-        style={styles.backgroundImage}
-        resizeMode={'cover'}
-    >
-
-    <SearchBar />
+    let textColor = backgroundImage !== sunny ? 'white' : 'black'
 
     
 
-    <View style={styles.headerText} >
-        <Text style={ ...styles.headerText, color:textColor, filter: }>{name}</Text>
-        <Text style={ ...styles.headerText, color:textColor}>{main}</Text>
-        <Text style={ ...styles.headerText, color:textColor}>{temp} °C</Text>
-    </View>
 
-    <View style={{flexDirection: 'row'}}>
+    
+    return (
+        <View style={styles.container}>
+            <StatusBar backgroundColor='darkgray' />
+            <ImageBackground 
+                source={backgroundImage}
+                style={styles.backgroundImg}
+                resizeMode='cover'
+            >
+                <SearchBar fetchWeatherData={fetchWeatherData} />
 
-        <View style={styles.extraInfo}>
-            <Text style={{fontSize: 22, color: 'white'}}>Humidity</Text>
-            <Text style={{fontSize: 22, color: 'white'}}>{humidity} %</Text>
+                <View style={{alignItems: 'center' }}>
+                    <Text style={{ ...styles.headerText, color: textColor, fontWeight: 'bold', fontSize: 46, marginTop: 100 }}>{name}</Text>
+                    <Text style={{ ...styles.headerText, color: textColor, fontWeight: 'bold'}}>{main}</Text>
+                    <Text style={{ ...styles.headerText, color: textColor, }}>{temp}°C</Text>
+                </View>
+
+                <View style={styles.extraInfo}>
+
+                    <View style={styles.info}>
+                        <Text style={{ fontSize: 22, color: 'white' }}>Humidity</Text>
+                        <Text style={{ fontSize: 22, color: 'white' }}>{humidity} %</Text>
+                    </View>
+
+                    <View style={styles.info}>
+                        <Text style={{ fontSize: 22, color: 'white' }}>Wind Speed</Text>
+                        <Text style={{ fontSize: 22, color: 'white' }}>{speed} m/s</Text>
+                    </View>
+                
+                </View>
+                
+
+            </ImageBackground>
         </View>
-
-        <View style={styles.info}>
-            <Text style={{fontSize: 22, color: 'white'}}>Wind Speed</Text>
-            <Text style={{fontSize: 22, color: 'white'}}>{speed} m/s</Text>
-        </View>
-
-    </View>
-
-
-
-    </ImageBackground>
-
-    </View>
-  )
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
     },
-    backgroundImage: {
-        flex:1,
-        width: Dimensions.get('screen').width,
-        height: '100%',
+    backgroundImg: {
+        flex: 1,
+        width: Dimensions.get('screen').width
     },
     headerText: {
-        fontSize: 40,
+        fontSize: 36,
         marginTop: 10,
-        textAlign: 'center',
-        
-        
-        },
+    },
     extraInfo: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        marginTop: 20,
+        justifyContent: 'space-between',
+        padding: 10
+    },
+    info: {
+        width: Dimensions.get('screen').width/2.5,
+        backgroundColor: 'rgba(0,0,0, 0.5)',
+        padding: 10,
+        borderRadius: 15,
+        justifyContent: 'center'
     }
+});
+
+// import React, {useState, useEffect} from 'react'
+// import { View, Text, StyleSheet, ImageBackground, Dimensions, StatusBar, ListViewComponent } from 'react-native'
+// import SearchBar from './SearchBar';
+// import { haze, rainy, snow, sunny } from '../assets/background/index';
+
+
+// export default function Weather({weatherData}) {
+
+//     const [backgroundImage, setBackgroundImage] = useState(null);
+//     const { weather,
+//             name,
+//             main: { temp, humidity },
+//             wind: { speed }
+//     } = weatherData;
+//     const [{ main }] = weather;
+
+//     useEffect(() => {
+//         setBackgroundImage(getBackgroundImg(main));
+//     }, [weatherData])
+
+//     function getBackgroundImg(weather) {
+//         if(weather === 'Snow') return snow
+//         if(weather === 'Clear') return sunny
+//         if(weather === 'Rain') return rainy
+//         if(weather === 'Haze') return haze
+//         return haze;
+//     }
+
+//     var textColor = backgroundImage !== sunny ? 'white' : 'orange'; 
+    
+
+//   return (
+//     <View style={styles.container}>
+
+//     <ImageBackground
+//         source={backgroundImage}
+//         style={styles.backgroundImage}
+//         resizeMode={'cover'}
+//     >
+
+//     <SearchBar />
+
+    
+
+//     <View style={styles.headerText} >
+//         <Text style={ ...styles.headerText, color:textColor, filter: }>{name}</Text>
+//         <Text style={ ...styles.headerText, color:textColor}>{main}</Text>
+//         <Text style={ ...styles.headerText, color:textColor}>{temp} °C</Text>
+//     </View>
+
+//     <View style={{flexDirection: 'row'}}>
+
+//         <View style={styles.extraInfo}>
+//             <Text style={{fontSize: 22, color: 'white'}}>Humidity</Text>
+//             <Text style={{fontSize: 22, color: 'white'}}>{humidity} %</Text>
+//         </View>
+
+//         <View style={styles.info}>
+//             <Text style={{fontSize: 22, color: 'white'}}>Wind Speed</Text>
+//             <Text style={{fontSize: 22, color: 'white'}}>{speed} m/s</Text>
+//         </View>
+
+//     </View>
+
+
+
+//     </ImageBackground>
+
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//     },
+//     backgroundImage: {
+//         flex:1,
+//         width: Dimensions.get('screen').width,
+//         height: '100%',
+//     },
+//     headerText: {
+//         fontSize: 40,
+//         marginTop: 10,
+//         textAlign: 'center',
+        
+        
+//         },
+//     extraInfo: {
+//         flexDirection: 'row'
+//     }
   
-})
+// })
